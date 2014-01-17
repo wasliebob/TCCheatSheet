666459a9508ab6d94db6506b07ae58fdb0dab0c7
wasliebob <wasliebob@live.nl>
2014-01-17 16:05:07 +0100

    Update extras.js

--- website/research/extras.js
+++ website/research/extras.js
@@ -5,10 +5,33 @@ var research_extras = [
                 mod: "thaumcraft_extras",
                 aspects: [ "Ordo", "Lucrum" ],
                 requirements: [],
-                recipes: {
-                },
+                recipes: {},
                 nature: "Important part of future recipe's",
                 video: "",
                 description: ""
-        }
+        },
+        
+         {
+                name: "Essence of Magic",
+                lore: "",
+                mod: "thaumcraft_extras",
+                aspects: [ "Ordo", "Praecantatio", "Metallum" ],
+                requirements: ["extras:Essence of Greed"],
+                recipes: {},
+                nature: "Important part of future recipe's",
+                video: "",
+                description: ""
+        },
+        
+        {
+                name: "Water Block",
+                lore: "",
+                mod: "thaumcraft_extras",
+                aspects: [ "Praecantatio", "Aqua"],
+                requirements: [],
+                recipes: {},
+                nature: "Compact water source block",
+                video: "",
+                description: ""
+        },
 ];
