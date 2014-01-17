bbd7a0c8a4ec8f3373e0dd32edcba78693a90d17
wasliebob <wasliebob@live.nl>
2014-01-17 15:56:04 +0100

    Started working on the Thaumcraft Extras Page

--- website/research/extras.js
+++ website/research/extras.js
@@ -1,3 +1,14 @@
 var research_extras = [
-
+ {
+                name: "Essence of Greed",
+                lore: "",
+                mod: "thaumcraft_extras",
+                aspects: [ "Ordo", "Lucrum" ],
+                requirements: [],
+                recipes: {
+                },
+                nature: "Important part of future recipe's",
+                video: "",
+                description: ""
+        }
 ];
