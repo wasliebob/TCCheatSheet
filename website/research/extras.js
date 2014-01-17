1f096510bd25a86bd7dc9c142ef9064bd2a7a136
wasliebob <wasliebob@live.nl>
2014-01-17 15:52:12 +0100

    Started working on the Thaumcraft Extras Page

--- website/research/extras.js
+++ website/research/extras.js
@@ -1,3 +1,17 @@
 var research_extras = [
 
+ {
+                name: "Essence of Greed",
+                lore: "Gold at its best",
+                mod: "thaumcraft_extras",
+                aspects: ["Ordo", "Lucrum"],
+                requirements: [],
+                recipes: {
+
+                },
+                nature: "Important for future recipe's.",
+                video: "",
+                description: "In your experimentation, you have discovered a special kind of gold fusion that leaves behind a strange amalgamation of precious metal. While useless for conventional crafting, the Pech place a high value on it, commanding very respectable trades. You have also noticed that these amalgamations fuse with each other, becoming bigger and more refined each time, for even better trading value."
+        },
+        
 ];
