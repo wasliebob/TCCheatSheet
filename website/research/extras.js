33f440bba713dc60521e6f90ff7c47d784f9574d
wasliebob <wasliebob@live.nl>
2014-01-17 16:13:35 +0100

    Update extras.js

--- website/research/extras.js
+++ website/research/extras.js
@@ -34,4 +34,40 @@ var research_extras = [
                 video: "",
                 description: ""
         },
+        
+        {
+                name: "Fire Block",
+                lore: "",
+                mod: "thaumcraft_extras",
+                aspects: [ "Ignis", "Praecantatio"],
+                requirements: [],
+                recipes: {},
+                nature: "Sets mobs on fire when steped on it.",
+                video: "",
+                description: ""
+        },
+        
+        {
+                name: "Light Block",
+                lore: "",
+                mod: "thaumcraft_extras",
+                aspects: [ "Lux", "Praecantatio"],
+                requirements: [],
+                recipes: {},
+                nature: "A block emitting light (this block has 16 different colors",
+                video: "",
+                description: ""
+        },
+        
+        {
+                name: "Ender Block",
+                lore: "",
+                mod: "thaumcraft_extras",
+                aspects: [ "Perditio", "Praecantatio", "Motus", "Iter", "Alienis"],
+                requirements: [],
+                recipes: {},
+                nature: "When clicked on this block you get teleporte random stack two on top of each other or bigger range",
+                video: "",
+                description: ""
+        },
 ];
